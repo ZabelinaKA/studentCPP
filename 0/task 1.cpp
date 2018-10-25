2b23c1148f412f3cb176381e2d97feefad532eb2
ZabelinaKA <43638368+ZabelinaKA@users.noreply.github.com>
2018-10-25 12:39:54 +0500

    Rename task 1.cpp to 0/task 1.cpp

---
+++ 0/task 1.cpp
@@ -0,0 +1,13 @@
+﻿// task 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Kseniya Zabelina"; 
+}
+
+// 
