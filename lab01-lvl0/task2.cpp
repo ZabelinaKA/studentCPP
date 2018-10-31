2dcbcd3f8b6cb3e5b496403280d32940e267382a
ZabelinaKA <43638368+ZabelinaKA@users.noreply.github.com>
2018-10-31 21:55:36 +0500

    Update task2.cpp

--- lab01-lvl0/task2.cpp
+++ lab01-lvl0/task2.cpp
@@ -7,14 +7,30 @@ using namespace std;
 
 int main()
 {
-   
-   cout << "What your name?" << endl;
+	cout << "What your name?" << endl;
 	char myname[256]= "";
-	int age;
+	int y, d, y_t, age, m, m_t, d_t, i=0;
 	cin >> myname;
-	cout << "How old are you?" << endl;
-	cin >> age;
+	cout << "When are you born?" << endl;
+	cout << "year: ";
+	cin >> y;
+	cout << "mounth: ";
+	cin >> m;
+	cout << "day: ";
+	cin >> d;
+	cout << "day today: ";
+	cin >> d_t;
+	cout << "mounth today: ";
+	cin >> m_t;
+	cout << "year today: ";
+	cin >> y_t;
+	age = y_t - y;
+	if (d_t - d < 0) {
+		i++;
+	}
+	if ((m_t - m < 0) || (i > 0)) {
+		age--;
+	}
 	cout << "Hello, " << myname << ". You're " << age << " years old." << endl;
-   
 }
 
