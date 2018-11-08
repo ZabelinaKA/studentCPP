ed9398fe60c01042b19115e3989046ebdf466766
ZabelinaKA <43638368+ZabelinaKA@users.noreply.github.com>
2018-11-08 12:57:13 +0500

    Rename task1.cpp to lab02-lvl0/task1.cpp

---
+++ lab02-lvl0/task1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a1, a2, a3, a4, a5, f1, f2, f3, f4, f5, b;
+	cout << "enter 5 numbers\n";
+	cin >> a1 >> a2 >> a3 >> a4 >> a5;
+	f1 = a1 * a1;
+	f2 = a2 * a2;
+	f3 = a3 * a3;
+	f4 = a4 * a4;
+	f5 = a5 * a5;
+	cout << f1 << endl << f2 << endl << f3 << endl << f4 << endl << f5 << endl;
+	b = f1 * f3 - f2 / f4 + f4 * (f5 - f1) - f2;
+	cout << b << endl;
+	cin.get();
+	return 0;
+}
+
+    
