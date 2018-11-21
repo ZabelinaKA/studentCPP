6a8bc607be83cd96c2d3dd56d01e986e492399c7
ZabelinaKA <43638368+ZabelinaKA@users.noreply.github.com>
2018-11-21 18:53:02 +0500

    Update Task1.cpp

--- lab01- lvl0/Task1/Task1.cpp
+++ lab01- lvl0/Task1/Task1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
    cout << "Hello, stranger.\n ";
-	cout << "This is your first program, call me(Laboratory 1 \ Task 1). \n ";
-	cout << "Yours, C++";
+   cout << "This is your first program, call me(Laboratory 1 \ Task 1). \n ";
+   cout << "Yours, C++";
 }
 
