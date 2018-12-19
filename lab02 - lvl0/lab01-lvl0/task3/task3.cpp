207a9d322a9377e2e57ed5a06d5c2e76d2321180
ZabelinaKA <43638368+ZabelinaKA@users.noreply.github.com>
2018-12-19 22:35:18 +0500

    Update task3.cpp

--- lab02 - lvl0/lab01-lvl0/task3/task3.cpp
+++ lab02 - lvl0/lab01-lvl0/task3/task3.cpp
@@ -7,32 +7,30 @@ using namespace std;
 
 int main()
 {
-	
-	while (true) {
-		int numToCalculateFactorial, stepNumber = 0;
-		int f = 1, num;
-		cout << "Type the number to calculate factorial :";
-		cin >> numToCalculateFactorial;
+  while (true) {
+	int numToCalculateFactorial, stepNumber = 0;
+	int f = 1, num;
+	cout << "Type the number to calculate factorial :";
+	cin >> numToCalculateFactorial;
+	do {
+	     f *= numToCalculateFactorial--;
+	   } while (numToCalculateFactorial > 0);
+	cout << "Number to compare:";
+	cin >> num;
+	cout << "Did the factorial exceed the value?";
+	if (f > num) {
+		numToCalculateFactorial = 1;
 		do {
-			f *= numToCalculateFactorial--;
-		} while (numToCalculateFactorial > 0);
-		cout << "Number to compare:";
-		cin >> num;
-		cout << "Did the factorial exceed the value?";
-
-		if (f > num) {
-			numToCalculateFactorial = 1;
-			do {
-				f /= numToCalculateFactorial++;
-				stepNumber = stepNumber++;
-			} while (f > num);
-			cout << " YES" << endl;
-			cout << "The step number is ";
-			cout << stepNumber << endl;
-		}
-		else if (f <= num) {
-			cout << "NO" << endl;
-		}
+			f /= numToCalculateFactorial++;
+			stepNumber = stepNumber++;
+		} while (f > num);
+		cout << " YES" << endl;
+		cout << "The step number is ";
+		cout << stepNumber << endl;
 	}
+	else if (f <= num) {
+		cout << "NO" << endl;
+	}
+  }
 }
 
