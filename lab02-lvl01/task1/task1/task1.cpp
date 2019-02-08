5b2f9f7cffedb36e0b54433382ddd2760139f556
ZabelinaKA <43638368+ZabelinaKA@users.noreply.github.com>
2019-02-08 18:54:42 +0500

    Update task1.cpp

--- lab02-lvl01/task1/task1/task1.cpp
+++ lab02-lvl01/task1/task1/task1.cpp
@@ -7,23 +7,49 @@ using namespace std;
 
 int main()
 {
-		char c = 'e', op;
-	bool k=true;
+	int n = 100, i = 0, j = 0;
 	cout << "Input constants:\n";
-	do
+	int *c = nullptr;
+	c = new int[n];
+	for ((c[i] != 0); i < n; i++)
 	{
-		cin >> &c;
+		cin >> c[i];
+		if ((char)c[i] == 0) {
+			cout << "your enter: end" << endl;
 
-		if (c == 'e') 
-		{
-			k = false;
+			break;
 		}
-	} while (k == true);
-	cout << "Input operations: ";
-	cin >> &op;
-	char * pA = &c;
-	char * pA = &op;
-	// *Pa +=10;
-	// cout << a ot *a
-	// array?
+	}
+	for (i = 0; c[i] != 0; i++)
+	{
+		j = i;
+		cout << c[j] << " ";
+	}
+	//c[i] = NULL;
+	cout << "input operation: ";
+	char z;
+	int S = 0;
+	for (j = 0; c[j + 1] != 0; j++)
+	{
+		cin >> z;
+		cout << "your enter operations: " << z << " ";
+		if (z == '+') {
+			S += c[j] + c[j+1];
+		}
+		else if (z == '-') {
+			S += c[j] - c[j + 1];
+		}
+		else if (z == '*') {
+			S += c[j] * c[j + 1];
+		}
+		else if (z == '/') {
+			S += c[j] / c[j + 1];
+		} 
+	}
+	cout << "result: " << S << endl;
+	system("pause");
+  return 0;
 }
+
+
+
